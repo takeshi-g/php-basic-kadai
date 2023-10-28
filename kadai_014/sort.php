79a59e6f12f429cfd64ebb84d173beffa7baaa36
Takeshi-G <takeshi-g@hotmail.com>
2023-10-28 11:47:31 +0900

    sort

--- kadai_014/sort.php
+++ kadai_014/sort.php
@@ -1,28 +1,31 @@
 <!DOCTYPE html>
 <html lang="ja">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
 </head>
+
 <body>
     <?php
-        $nums = [15, 4, 18, 23, 10 ];
-        function sort_2way ($array, $order) {
-            if($order === TRUE) {
-                echo '昇順にソートします。'  . '<br>';
-                sort($array);
-                
-            } else {
-                echo '降順にソートします。'  . '<br>';
-                rsort($array);
-            }
-            foreach($array as $value) {
-                echo $value . '<br>';
-            }
-            
+    $nums = [15, 4, 18, 23, 10];
+    function sort_2way($array, $order)
+    {
+        if ($order === TRUE) {
+            echo '昇順にソートします。'  . '<br>';
+            sort($array);
+        } else {
+            echo '降順にソートします。'  . '<br>';
+            rsort($array);
         }
-        sort_2way($nums, TRUE);
+        foreach ($array as $value) {
+            echo $value . '<br>';
+        }
+    }
+    sort_2way($nums, TRUE);
+    sort_2way($nums, FALSE);
     ?>
 </body>
+
 </html>
